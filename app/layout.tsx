3c1dadf52c505de9f77ac499663b5c6eb2b04681
shishir <shishirtech506@gmail.com>
2025-04-30 18:48:38 +0530

    added bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className="dark">
-      <body className={`${Bricol.className} antialiased`}>{children}</body>
+    <html lang='en' className='dark'>
+      <body className={`${Bricol.className} antialiased pattern`}>{children}</body>
     </html>
   );
 }
