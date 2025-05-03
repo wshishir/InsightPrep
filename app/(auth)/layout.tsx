c1efc43eeda0adc87497fa09622155832f88bcb5
shishir <shishirtech506@gmail.com>
2025-05-04 01:06:19 +0530

    added auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 
-const AuthLayout = ({children} :{children : ReactNode}) => {
-  return (
-    <div>{children}</div>
-  )
-}
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return <div className='auth-layout'>{children}</div>;
+};
 
-export default AuthLayout
+export default AuthLayout;
