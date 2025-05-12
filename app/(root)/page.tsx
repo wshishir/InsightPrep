4373ca904b84ef390a8d0fd6b55942e395d9c67a
shishir <shishirtech506@gmail.com>
2025-05-13 05:07:28 +0530

    landing page navbar done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import React from 'react';
 
 const page = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <>
+      {/*Navbar*/}
+      <section className='mt-6'>
+        <div className='flex justify-between'>
+          <span className='ml-18 text-2xl font-medium'>InsightPrep</span>
+          <nav className='hidden md:flex gap-6 justify-center w-full text-lg'>
+            <a href='#'>Features</a>
+            <a href='#'>Pricing</a>
+            <a href='#'>Contact</a>
+          </nav>
+          <Button className='mr-20'>Login</Button>
+        </div>
+      </section>
+      {/*Hero Section*/}
+      <section>
+        
+      </section>
+    </>
+  );
+};
 
-export default page
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Bricol } from "@/app/ui/fonts";
-import { Toaster } from "sonner";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Bricol } from '@/app/ui/fonts';
+import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
-  title: "InsightPrep",
-  description: "AI powered platform for mock interviews",
+  icons: ['/avatar.png'],
+  title: 'InsightPrep',
+  description: 'AI powered platform for mock interviews',
 };
 
 export default function RootLayout({
